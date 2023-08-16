573f7cf63a780229744b6a7bbc98ec32d7564af9
zaki-20 <zakigame199@gmail.com>
2023-08-16 12:17:14 +0500

    reduxtoolkit setup complete

---
+++ src/Redux/features/cocktailSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchCocktails = createAsyncThunk("cocktails/fetchCocktails",
+    async () => {
+        try {
+            const res = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=");
+            return res
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+);
+
+const cocktailSlice = createSlice({
+    name: "cocktails",
+    initialState: {
+        loading: false,
+        cocktails: [],
+        error: null,
+        cocktail: [],
+    },
+
+    extraReducers: {
+        [fetchCocktails.pending]: (state, action) => {
+            state.loading = true;
+        },
+        [fetchCocktails.fulfilled]: (state, action) => {
+            state.loading = false;
+            state.cocktails = action.payload.drinks;
+        },
+        [fetchCocktails.rejected]: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+    }
+})
+
+export default cocktailSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <BrowserRouter>
+    <Provider store={store}>
     <App />
-  </BrowserRouter>
+   </Provider>
+  </BrowserRouter >
 
 );
